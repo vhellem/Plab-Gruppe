9142d4820ebc51f1511246dbdc8b04411aa1df62
Vegard Hellem <vhellem@gmail.com>
2015-10-22 18:18:28 +0200

    -r

--- Robot/basic_robot/Behaviours/attackRed.py
+++ Robot/basic_robot/Behaviours/attackRed.py
@@ -10,11 +10,13 @@ class AttackRed(Behaviour):
     def __init__(self, pri=10):
         self.sensor = CamUltra()
         self.weight = 0
+        self.active = True
 
     def sense_and_act(self):
         s = 1
         im = self.sensor.image
         if im:
+            im.dump_image("test.jpeg")
             if im.red():
                 self.recommendation = [1, 1, False]
                 self.weight = 10
